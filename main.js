c762d61320d78acb4d376abcb9a860904259feff
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2022-03-31 17:48:58 -0300

    add slideshow gallery

--- main.js
+++ main.js
@@ -5,3 +5,24 @@ toggleBtn.addEventListener('click', ()=> {
     toggleLinks.classList.toggle('active');
     console.log('workddddssss')    
 })
+
+let slideshows = document.querySelectorAll('[data-component="slideshow"]');
+
+slideshows.forEach(initSlideShow);
+
+function initSlideShow(slideshow) {
+    let slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`);
+    let index = 0, time = 5000;
+    slides[index].classList.add('active');
+
+    setInterval( () => {
+        slides[index].classList.remove('active');
+
+        // go over each slide incrementing the index
+        index++;
+
+        if (index === slides.length) index = 0;
+
+        slides[index].classList.add('active');
+    }, time);
+}
