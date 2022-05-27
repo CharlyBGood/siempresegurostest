5c6c33bf6bcad86886940674f547336a4129e3af
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2022-05-27 16:12:46 -0300

    fix php

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php 
 
 
-$conex = mysqli_connect("localhost", 'c2560614', 'PpMnMoAs01', 'c2560614_seguros');
+$conex = mysqli_connect("localhost", 'c2560614_seguros', 'PpMnMoAs01', 'c2560614_seguros');
 
 
 $name = $_POST['nombre'];
@@ -10,15 +10,16 @@ $email = $_POST['email'];
 $tel = $_POST['telefono'];
 $datos = $_POST['datos'];
 
+$send = $_POST['enviar'];
 
 
-$sql = "INSERT INTO seguros_form(Id, Nombre, postalCod, Email, Tel, Datos) VALUES ('0', '$name', '$postalCod,'$email', '$tel', '$datos')";
+$sql = "INSERT INTO seguros_form(Nombre, postalCod, Email, Tel, Datos) VALUES ('$name', '$postalCod','$email', '$tel', '$datos')";
 
 $result = mysqli_query($conex, $sql);
 
 if ($result) {
-    echo "Recorded on your mind";
-}
+    echo "pasó la prueba";
+} 
 
 
 ?>
