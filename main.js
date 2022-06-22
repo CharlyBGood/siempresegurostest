eaa4f4d73c4105c7cc3d5055ebf5364222bd250f
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2022-06-22 19:30:01 -0300

    validate form inputs

--- main.js
+++ main.js
@@ -76,20 +76,14 @@ span1.onclick = function () {
   document.body.style.overflow = "auto";
 };
 
-quienesBtn.addEventListener('click', openOne);
+quienesBtn.addEventListener("click", openOne);
 
 function openOne() {
   modalOne.style.display = "block";
   document.body.style.overflow = "hidden";
-};
-
-// quienesBtn.onclick = function () {
-//   modalOne.style.display = "block";
-//   document.body.style.overflow = "hidden";
-// };
+}
 
 // MODAL ONE CONTAINER
-
 
 // MODAL TWO CONTAINER
 let modalTwo = document.getElementById("myModalTwo");
@@ -103,17 +97,12 @@ span.onclick = function () {
   document.body.style.overflow = "auto";
 };
 
-segurosBtn.addEventListener('click', openTwo);
+segurosBtn.addEventListener("click", openTwo);
 
 function openTwo() {
   modalTwo.style.display = "block";
   document.body.style.overflow = "hidden";
-};
-
-// segurosBtn.onclick = function () {
-//   modalTwo.style.display = "block";
-//   document.body.style.overflow = "hidden";
-// };
+}
 
 // MODAL TWO CONTAINER
 
@@ -130,17 +119,12 @@ span3.onclick = function () {
   document.body.style.overflow = "auto";
 };
 
-descargaBtn.addEventListener('click', openThree);
+descargaBtn.addEventListener("click", openThree);
 
 function openThree() {
   modalThree.style.display = "block";
   document.body.style.overflow = "hidden";
-};
-
-// descargaBtn.onclick = function () {
-//   modalThree.style.display = "block";
-//   document.body.style.overflow = "hidden";
-// };
+}
 
 // MODAL THREE CONTAINER
 
@@ -157,17 +141,12 @@ span4.onclick = function () {
   document.body.style.overflow = "auto";
 };
 
-contactoBtn.addEventListener('click', openFour);
+contactoBtn.addEventListener("click", openFour);
 
 function openFour() {
   modalFour.style.display = "block";
   document.body.style.overflow = "hidden";
-};
-
-// contactoBtn.onclick = function () {
-//   modalFour.style.display = "block";
-//   document.body.style.overflow = "hidden";
-// };
+}
 
 // close when click out of modal container
 
@@ -186,67 +165,143 @@ window.onclick = function (event) {
   }
 };
 
-
 // MODALS TEST
 
 let modalMenu = document.getElementsByClassName("modal-menu");
 
-
-// for (let el of modalMenu) {
-  
-//   el.addEventListener("click", function(ev) {
-//     console.log("atrevido");
-// }
-
 // MODALS TEST
 
+let linksNav = document.querySelectorAll(".modal-menu");
 
-let linksNav = document.querySelectorAll('.modal-menu');
-
-linksNav[4].addEventListener('click', openModalOne);
-linksNav[8].addEventListener('click', openModalOne);
-linksNav[12].addEventListener('click', openModalOne);
+linksNav[4].addEventListener("click", openModalOne);
+linksNav[8].addEventListener("click", openModalOne);
+linksNav[12].addEventListener("click", openModalOne);
 
 function openModalOne() {
-  console.log('funciona');
+  console.log("funciona");
   openOne();
-  modalTwo.style.display = 'none';
-  modalThree.style.display = 'none';
-  modalFour.style.display = 'none';
+  modalTwo.style.display = "none";
+  modalThree.style.display = "none";
+  modalFour.style.display = "none";
 }
 
-linksNav[1].addEventListener('click', openModalTwo);
-linksNav[9].addEventListener('click', openModalTwo);
-linksNav[13].addEventListener('click', openModalTwo);
+linksNav[1].addEventListener("click", openModalTwo);
+linksNav[9].addEventListener("click", openModalTwo);
+linksNav[13].addEventListener("click", openModalTwo);
 
 function openModalTwo() {
-  console.log('funciona 2');
+  console.log("funciona 2");
   openTwo();
-  modalOne.style.display = 'none';
-  modalThree.style.display = 'none';
-  modalFour.style.display = 'none';
+  modalOne.style.display = "none";
+  modalThree.style.display = "none";
+  modalFour.style.display = "none";
 }
 
-linksNav[2].addEventListener('click', openModalThree);
-linksNav[6].addEventListener('click', openModalThree);
-linksNav[14].addEventListener('click', openModalThree);
+linksNav[2].addEventListener("click", openModalThree);
+linksNav[6].addEventListener("click", openModalThree);
+linksNav[14].addEventListener("click", openModalThree);
 
 function openModalThree() {
-  console.log('funciona 3');
+  console.log("funciona 3");
   openThree();
-  modalOne.style.display = 'none';
-  modalTwo.style.display = 'none';
-  modalFour.style.display = 'none';
+  modalOne.style.display = "none";
+  modalTwo.style.display = "none";
+  modalFour.style.display = "none";
 }
 
-linksNav[3].addEventListener('click', openModalFour);
-linksNav[7].addEventListener('click', openModalFour);
-linksNav[11].addEventListener('click', openModalFour);
+linksNav[3].addEventListener("click", openModalFour);
+linksNav[7].addEventListener("click", openModalFour);
+linksNav[11].addEventListener("click", openModalFour);
 
 function openModalFour() {
-  console.log('funciona 4');
+  console.log("funciona 4");
   openFour();
-  modalOne.style.display = 'none';
-  modalTwo.style.display = 'none';
-  modalThree.style.display = 'none';
+  modalOne.style.display = "none";
+  modalTwo.style.display = "none";
+  modalThree.style.display = "none";
+}
+
+//  ---------------------------------
+
+// form validation two
+
+const form = document.getElementById("form");
+
+const nameField = document.getElementById("nombre");
+const postCode = document.getElementById("postal");
+const emailAddress = document.getElementById("email");
+const tel = document.getElementById("telefono");
+const data = document.getElementById("datos");
+
+form.addEventListener("submit", (e) => {
+    checkInputs();
+    if (!validForm) {
+      e.preventDefault();
+      console.log('not sended')
+    } else {
+      console.log('sended');
+    }
+});
+
+const expresiones = {
+  emailIsValid: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  nameIsValid: /^[a-zA-Z\s]+$/,
+  telIsValid: /^\d{6,14}$/,
+  postCodValid: /^\d{3,7}$/
+}
+
+
+function checkInputs() {
+  const formData = {
+    nameFieldValue: nameField,
+    postCodeValue: postCode,
+    emailAddressValue: emailAddress,
+    telValue: tel,
+    dataValue: data,
+  };
+
+  Object.keys(formData).forEach((key) => {
+    if (formData[key].value === "") {
+      // show error
+      // add class
+      setErrorFor(formData[key], "Debes completar este campo");
+    } else {
+      // add success class
+      setSuccessFor(formData[key]);
+    }
+
+    if (!expresiones.emailIsValid.test(emailAddress.value)) {
+      setErrorFor(emailAddress, "¡El email no es válido!");
+    }
+    
+    if (!expresiones.nameIsValid.test(nameField.value)) {
+      setErrorFor(nameField, "¡El campo de nombre no debe contener números ni símbolos!");
+    }
+    
+    if (!expresiones.telIsValid.test(tel.value)) {
+      setErrorFor(tel, "¡Debes escribir un Número de Tel. válido!");
+    }
+
+    if (!expresiones.postCodValid.test(postCode.value)) {
+      setErrorFor(postCode, "¡Debes escribir un Código Postal válido!");
+    }
+  });
+}
+
+let validForm = true;
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement; // .form-control
+  const small = formControl.querySelector("small");
+  // add error message inside small
+  small.innerText = message;
+  // add error class
+  formControl.className = "form-control error";
+  validForm = false;
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement; // .form-control
+  formControl.className = "form-control success";
+  validForm = true;
 }
